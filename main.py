b09690ac650aca5d1d152f23806ba6bceadae1e3
Mohamed Shaad <98437584+shaadclt@users.noreply.github.com>
2023-11-29 00:26:58 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from langchain_helper import get_few_shot_db_chain
+
+st.title("Shaad T-Shirts: Database Q&A")
+
+question = st.text_input("Question: ")
+
+if question:
+    chain = get_few_shot_db_chain()
+    response = chain.run(question)
+
+    st.header("Answer")
+    st.write(response)
+
+
+
+
+
+
