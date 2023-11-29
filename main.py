931debc1fc8ce58da3dc5dbadbb9416a78395da9
Mohamed Shaad <98437584+shaadclt@users.noreply.github.com>
2023-11-29 11:05:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from langchain_helper import get_few_shot_db_chain
 
-st.title("Retail Database Assistant")
+st.title("Retail Store Database Assistant")
 
 question = st.text_input("Question: ")
 
